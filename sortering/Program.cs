af8dc62ede670c4cdadf54340177fa99b60cff4e
GustavJohansson420 <89460949+GustavJohansson420@users.noreply.github.com>
2022-02-08 15:48:03 +0100

    Add files via upload

---
+++ sortering/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace sortering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int räknare=0;
+            while (räknare <=1000)
+            {
+                Random slump=new Random();
+                int rand = slump.Next(1,1001);
+                Console.WriteLine(rand);
+                räknare=räknare+1;
+            }
+        }
+    }
+}
